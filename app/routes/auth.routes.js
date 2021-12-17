c0a33a7f2f593882f8d060e8b3baeda475759f02
Arqam Rafay <arqam.rafay@net.sixlogs.com>
2021-12-17 11:31:49 +0500

    'f'

---
+++ app/routes/auth.routes.js
@@ -0,0 +1,31 @@
+const { verifySignUp } = require("../middleware");
+const controller = require("../controllers/auth.controller");
+
+module.exports = function (app) {
+  app.use(function (req, res, next) {
+    res.header(
+      "Access-Control-Allow-Headers",
+      "x-access-token, Origin, Content-Type, Accept"
+    );
+    next();
+  });
+
+  //   http://localhost:8080/api/auth/signup
+  //   {
+  //     "username": "mod",
+  //     "email": "abc@gmail.com",
+  //     "password": "12345678",
+  //     "roles": ["moderator", "user"]
+  //   }
+
+  app.post(
+    "/api/auth/signup",
+    [
+      verifySignUp.checkDuplicateUsernameOrEmail,
+      verifySignUp.checkRolesExisted
+    ],
+    controller.signup
+  );
+
+  app.post("/api/auth/signin", controller.signin);
+};
---
+++ app/routes/user.routes.js
@@ -0,0 +1,34 @@
+const { authJwt } = require("../middleware");
+const controller = require("../controllers/user.controller");
+
+module.exports = function (app) {
+  app.use(function (req, res, next) {
+    res.header(
+      "Access-Control-Allow-Headers",
+      "x-access-token, Origin, Content-Type, Accept"
+    );
+    next();
+  });
+  // get: http://localhost:8080/api/test/all
+  app.get("/api/test/all", controller.allAccess);
+
+  // http://localhost:8080/api/test/user  : No token provided
+
+  app.get(
+    "/api/test/user",
+    [authJwt.verifyToken],
+    controller.userBoard
+  );
+
+  app.get(
+    "/api/test/mod",
+    [authJwt.verifyToken, authJwt.isModerator],
+    controller.moderatorBoard
+  );
+
+  app.get(
+    "/api/test/admin",
+    [authJwt.verifyToken, authJwt.isAdmin],
+    controller.adminBoard
+  );
+};
